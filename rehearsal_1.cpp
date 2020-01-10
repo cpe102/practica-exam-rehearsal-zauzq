26db40450fda5519f9dd3a53e897d181cddaaf02
lab302-14 <lab302-14@labeng.local>
2020-01-10 16:26:47 +0700

    done

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+int main()
+{
+    double B;
+    string A;
+    cout << "What is your name? : ";
+    cin >> A;
+    cout << "What is your GPA? : ";
+    cin >> B;
+    if( B >= 3.50){
+        cout << A <<" InW Jrim Jrim!!!";
+    }
+        else
+        cout << "Try harder, "<< A <<"!!!";
+}
