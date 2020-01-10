a59681dab19db96a42946720721966f981918440
neokarn <34811101+neokarn@users.noreply.github.com>
2020-01-10 14:37:14 +0700

    Initial commit

---
+++ rehearsal_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//Write sumSqrt() here.
+
+
+int main()
+{
+    double a = sumSqrt(20);
+    double b = sumSqrt(10);
+    double c = sumSqrt(5);
+    double d = sumSqrt(2);
+    double e = sumSqrt(1);
+    double f = sumSqrt(0);
+    double g = sumSqrt(-1);
+    
+    cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
+
+}
---
+++ rehearsal_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//Write compress() here.
+
+int main()
+{
+    string a = conpress("ABCDEFGHIJKLMN");
+    string b = conpress("123456");
+    string c = conpress("HelloWorld");
+    string d = conpress("BNK48");
+    string e = conpress("COMPROG261102");
+    string f = conpress("A");
+    string g = conpress("AB");
+    string h = conpress("ABC");
+    string i = conpress("ABCD");
+    
+    cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n" << h << "\n" << i << "\n";
+
+}
